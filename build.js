35df58df2fce082a05d289bb72c949a4a3ca7d87
Serban Andrei Stancu <dompuiu@users.noreply.github.com>
2021-05-18 14:28:02 -0600

    Update build.js

--- build.js
+++ build.js
@@ -216,7 +216,7 @@ module.exports = getDataElementValues => ({
         source: function (arc, utils) {
           return new Promise(function (resolve) {
             setTimeout(function () {
-              resolve('precious');
+              resolve('precious_new');
             }, 1);
           });
         }
